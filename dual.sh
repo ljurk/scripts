56d30664754bd3df2c5d9a72c904e4b8dd870c18
ljurk <ljurk@pm.me>
2018-12-03 21:23:01 +0100

    have to update my bg image after changing monitors

--- dual.sh
+++ dual.sh
@@ -2,3 +2,4 @@
 xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
 #!/bin/sh
 xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1680x1050 --pos 1920x0 --rotate normal
+nitrogen --restore
