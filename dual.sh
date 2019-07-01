6cf0eb3575662cdf98b77c2306d80f16389e0fbb
ljurk <ljurk@pm.me>
2019-07-01 06:26:27 +0200

    monior connection names changed

--- dual.sh
+++ dual.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
-#!/bin/sh
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1680x1050 --pos 1920x0 --rotate normal
+xrandr --output LVDS-1 --off --output VGA-1 --mode 1680x1050 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
 nitrogen --restore
